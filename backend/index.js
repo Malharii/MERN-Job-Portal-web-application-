b3c75e85d5c109c7b41ddab7438b5c69e0669ad0
Malharii <mrmalhari96@gmail.com>
2025-08-16 18:13:35 +0530

    Job Portal web appliction backend and frontend setup compled

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
+
+const app = express();
+
+// midlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = () => {
+  origin: "http//localhost:5173";
+  credential: true;
+};
+connectDB();
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is runing on ${PORT}`);
+});
