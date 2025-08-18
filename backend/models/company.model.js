9ed88476429249f7300cca090b59398e22212c28
Malharii <mrmalhari96@gmail.com>
2025-08-18 16:26:35 +0530

    company models is comolet

---
+++ backend/models/company.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    logo: {
+      type: String, // url of company logo
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const company = companySchema.model("Company", companySchema);
